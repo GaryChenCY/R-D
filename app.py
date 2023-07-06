c23d6d46a00184e4ad04ca312252d802e02458d2
GARYCHEN <jacl0977234207123@gmail.com>
2023-07-06 14:27:08 +0800

    Frist

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask
 app=Flask(__name__)
 
-@app.route("/")
-def index():
-    return "hi gary"
+import pdfplumber
+import xlwt
+workbook = xlwt.Workbook()
+sheet = workbook.add_sheet('Sheet1')
+i = 0
+pdf = pdfplumber.open("1.pdf")
+print('開始讀取數據')
+for page in pdf.pages:
+for table in page.extract_tables():
+for row in table:
+for j in range(len(row)):
+sheet.write(i, j, row[j])
+i += 1
+pdf.close()
+workbook.save('C:/Users/Administrator/Desktop/result.xls')
+print('保存成功！')
 
-app.run()
+if __name__ == '__main__':
+  window()
